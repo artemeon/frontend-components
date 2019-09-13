bc98f9d121c085a9f2c376025e975eabf4a9512f
Bernhard Grabietz <bernhard.grabietz@artemeon.de>
2019-09-13 15:17:38 +0200

    change $emit event name into change instead of input to have the same method with datepicker

--- src/agp-components/agp-form/agpInput/agpInput.ts
+++ src/agp-components/agp-form/agpInput/agpInput.ts
@@ -6,8 +6,8 @@ class agpInput extends Mixins(FormMixin) {
   inputVal = this.value
 
   @Watch('inputVal')
-  onChange (val: any) {
-    this.$emit('input', val)
+  onChange (value: any) {
+    this.$emit('change', value)
   }
 }
 export default agpInput
